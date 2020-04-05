06a780c8fde86aac95d20548c49955a5c066317f
Donovan Adams <donovan@hydrotik.com>
2020-04-05 10:13:06 -0400

    add another page

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next';
+
+
+const About: NextPage = () => (
+  <div>
+      <p>This is the about page</p>
+    </div>
+);
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
-const Home = () => <h1>Hello world!</h1>;
+import { NextPage } from 'next';
+
+const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
+	<div>
+		<h1>Hello world! - user agent: {userAgent}</h1>
+		<p><a href="/about">About</a></p>
+  	</div>
+);
+
+Home.getInitialProps = async ({ req }) => {
+  const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
+  return { userAgent };
+};
 
 export default Home;
