a6bada90230148e7126cc8fe1ca053d31e27e889
Donovan Adams <donovan@hydrotik.com>
2020-04-06 14:11:14 -0400

    remove csx nad add tradwinds from TextLoop component

--- src/components/TextLoop.tsx
+++ src/components/TextLoop.tsx
@@ -1,115 +1,31 @@
 import React, { useState, useEffect } from "react";
 import TextLoop from "react-text-loop";
 
-//@ts-ignore
-import cxs from "cxs/component";
+interface WrapperProps {
+  className?: string
+}
 
-const Example:any = cxs("div")({
-  fontSize: "24px"
-});
+const Example: React.FC<WrapperProps> = p => (
+  <div className={p.className}>{p.children}</div>
+)
 
-const Title = cxs("div")({
-  marginBottom: "5px",
-  fontSize: "10px",
-  fontWeight: 600,
-  textTransform: "uppercase",
-  color: "#aaa"
-});
+const Section: React.FC<WrapperProps> = p => (
+  <div className={p.className}>{p.children}</div>
+)
 
-const Section = cxs("div")({
-  marginBottom: "50px",
-  fontFamily:
-    '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif'
-});
-
-const StyledTextLoop = cxs(TextLoop)({
-  display: "block"
-});
-
+//TODO Abstract out the loop values as props and map to span
 const Masked = () => (
   <Section>
-    <Example>
+    <Example className="font-bold my-8 p-3 text-lg md:text-2xl">
       <TextLoop mask={true}>
         <span>Delight customers</span>
-        <span>Increase revenue</span>
+        <span>Increase ROI</span>
         <span>Exceed KPIs</span>
-        <span>Digital transformation</span>
+        <span>Tell stories</span>
+        <span>Revolutionize multichannel</span>
+        <span>Maximise disruption</span>
       </TextLoop>{" "}
-      in every category.
-    </Example>
-  </Section>
-);
-
-const Controlled = () => {
-  const [options, setOptions] = useState(["Trade faster", "Increase sales"]);
-  const [interval, setInterval] = useState(0);
-
-  useEffect(() => {
-    const optionsTimeout = setTimeout(() => {
-      setOptions([
-        "Trade faster",
-        "Increase sales",
-        "Stock winners",
-        "Price perfectly"
-      ]);
-      console.log("change options");
-    }, 10000);
-
-    return () => {
-      clearTimeout(optionsTimeout);
-    };
-  }, []);
-
-  useEffect(() => {
-    const intervalStartTimeout = setTimeout(() => {
-      console.log("start");
-      setInterval(1000);
-    }, 5000);
-    return () => {
-      clearTimeout(intervalStartTimeout);
-    };
-  }, []);
-
-  useEffect(() => {
-    const intervalStopTimeout = setTimeout(() => {
-      setInterval(0);
-      console.log("stop");
-    }, 15000);
-
-    return () => {
-      clearTimeout(intervalStopTimeout);
-    };
-  }, []);
-
-  return (
-    <Section>
-      <Title>Controlled props (start/stop animation and change options)</Title>
-      <Example>
-        <TextLoop interval={interval} children={options} /> in every category.
-      </Example>
-    </Section>
-  );
-};
-
-const Staggered = () => (
-  <Section>
-    <Title>Staggered (with delay prop and custom styling)</Title>
-    <Example>
-      <StyledTextLoop mask={true} fade={false}>
-        <span>Trade</span>
-        <span>Increase</span>
-        <span>Stock</span>
-      </StyledTextLoop>
-      <StyledTextLoop delay={500} mask={true} fade={false}>
-        <span>faster</span>
-        <span>sales</span>
-        <span>winners</span>
-      </StyledTextLoop>
-      <StyledTextLoop delay={1000} mask={true} fade={false}>
-        <span>in every category.</span>
-        <span>in something else.</span>
-        <span>in other category.</span>
-      </StyledTextLoop>
+      with digital transformation.
     </Example>
   </Section>
 );
