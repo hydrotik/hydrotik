eb5abb5811a0e2ea51313112e6407ba2ba7d9311
Donovan Adams <donovan@hydrotik.com>
2020-05-07 01:32:50 -0400

    visualization refactor datahelper logic to pass promises and remove some boilerplate

--- src/pages/visualization.tsx
+++ src/pages/visualization.tsx
@@ -35,11 +35,11 @@ type ParseProps = {
 
 type MyState = {
 	github: string;
-	totals: Array<object>;
-	borough: Array<string>;
-	age: Array<string>;
-	sex: Array<string>;
-};
+	totals: Array<Array<string>>;
+	borough: Array<Array<string>>;
+	age: Array<Array<string>>;
+	sex: Array<Array<string>>;
+}
 
 type NumberProps = {
 	value: number|string;
@@ -49,13 +49,13 @@ type NumberProps = {
 *	Helper functions and sanitizers
 */
 
-function numberWithCommas(x: string): string {
+function numberWithCommas(x: number): string {
 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 function NumberValue(props: NumberProps): JSX.Element {
 	const { value } = props;
-	return <span>{ typeof value === 'number' ? value : numberWithCommas(value) }</span>;
+	return <span>{ typeof value !== 'number' ? value : numberWithCommas(value) }</span>;
 }
 
 /*
@@ -68,94 +68,95 @@ class Visualization extends React.Component<MyProps, MyState> {
 
 		this.state = {
 			github: 'Loading data...',
-			totals: [['Loading data...', '']],
-			borough: [],
-			age: [],
-			sex: [],
+			totals: [['Loading data...'], []],
+			borough: [['Loading data...'], []],
+			age: [['Loading data...'], []],
+			sex: [['Loading data...'], []],
 		};
-
-		this.resolveCsvData = this.resolveCsvData.bind(this);
-		this.resolveGithubData = this.resolveGithubData.bind(this);
-		this.parseGithubData = this.parseGithubData.bind(this);
-		this.resolveBoroughData = this.resolveBoroughData.bind(this);
-		this.resolveAgeData = this.resolveAgeData.bind(this);
-		this.resolveSexData = this.resolveSexData.bind(this);
 	}
 
 	componentDidMount(): void {
-		const cvcb = this.resolveCsvData;
-
 		dc.getDataPromise('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/summary.csv')
+			.then((result: object) => {
+				let data;
+				Papa.parse(result.toString(), {
+					header: false,
+					dynamicTyping: true,
+					complete: (r) => {
+						data = r;
+					},
+				});
+				return data;
+			})
 			.then((result) => {
-				// eslint-disable-next-line no-console
-				console.log(result);
+				const { data } = (result as unknown) as DataProps;
+				const noAsteriks: Array<Array<string>> = JSON.parse(JSON.stringify(data).replace(/\*/g, ''));
+				this.resolveCsvData(noAsteriks);
 			});
 
-		dc.getData(
-			'https://raw.githubusercontent.com/nychealth/coronavirus-data/master/summary.csv',
-			(data: string): void => {
-				Papa.parse(data, {
+		dc.getDataPromise('https://api.github.com/repos/nychealth/coronavirus-data/commits')
+			.then((result: unknown) => {
+				const r = JSON.parse(result as string)[0];
+				const d = r.commit.author.date;
+				const n = moment(d).format('MM/DD/YYYY, hh:hh a');
+				const s = `Chart updated on ${n}`;
+				return s;
+			})
+			.then((result) => {
+				this.resolveGithubData(result);
+			});
+
+		dc.getDataPromise('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/boro.csv')
+			.then((result: object) => {
+				let data;
+				Papa.parse(result.toString(), {
 					header: false,
-					complete: cvcb,
 					dynamicTyping: true,
+					complete: (r) => {
+						data = r;
+					},
 				});
-			},
-		);
+				return data;
+			})
+			.then((result) => {
+				this.resolveBoroughData((result as unknown) as ParseResult);
+			});
 
-		const cghcb = this.resolveGithubData;
-		const ParseGithub = this.parseGithubData;
-
-		dc.getData(
-			'https://api.github.com/repos/nychealth/coronavirus-data/commits',
-			(data: string): void => {
-				ParseGithub(data, {
-					complete: cghcb,
-				});
-			},
-		);
-
-		const boroughcb = this.resolveBoroughData;
-
-		dc.getData(
-			'https://raw.githubusercontent.com/nychealth/coronavirus-data/master/boro.csv',
-			(data: string): void => {
-				Papa.parse(data, {
+		dc.getDataPromise('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/by-age.csv')
+			.then((result: object) => {
+				let data;
+				Papa.parse(result.toString(), {
 					header: false,
-					complete: boroughcb,
 					dynamicTyping: true,
+					complete: (r) => {
+						data = r;
+					},
 				});
-			},
-		);
+				return data;
+			})
+			.then((result) => {
+				this.resolveAgeData((result as unknown) as ParseResult);
+			});
 
-		const agecb = this.resolveAgeData;
-
-		dc.getData(
-			'https://raw.githubusercontent.com/nychealth/coronavirus-data/master/by-age.csv',
-			(data: string): void => {
-				Papa.parse(data, {
+		dc.getDataPromise('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/by-sex.csv')
+			.then((result: object) => {
+				let data;
+				Papa.parse(result.toString(), {
 					header: false,
-					complete: agecb,
 					dynamicTyping: true,
+					complete: (r) => {
+						data = r;
+					},
 				});
-			},
-		);
-
-		const sexcb = this.resolveSexData;
-
-		dc.getData(
-			'https://raw.githubusercontent.com/nychealth/coronavirus-data/master/by-sex.csv',
-			(data: string): void => {
-				Papa.parse(data, {
-					header: false,
-					complete: sexcb,
-					dynamicTyping: true,
-				});
-			},
-		);
+				return data;
+			})
+			.then((result) => {
+				this.resolveSexData((result as unknown) as ParseResult);
+			});
 	}
 
-	resolveCsvData(result: ParseResult): void {
-		this.setState({ totals: result.data });
+	resolveCsvData(result: Array<Array<string>>): void {
+		this.setState({ totals: result });
 	}
 
 	resolveGithubData(result: string): void {
@@ -193,6 +194,7 @@ class Visualization extends React.Component<MyProps, MyState> {
 			sex,
 			totals,
 		} = this.state;
+
 		return (
 			<Layout>
 				<main className="flex-1 w-full max-w-4xl p-4 mx-auto md:px-8 md:py-16">
@@ -206,39 +208,36 @@ class Visualization extends React.Component<MyProps, MyState> {
 							<p className="mb-6">{ github }</p>
 							<h2 className="font-bold text-xl">Borough Totals:</h2>
 							{
-								//	eslint-disable-next-line @typescript-eslint/no-explicit-any
-								borough.map((section: any) => (
-									<div key={section[0]}>
-										<strong>{section[0].replace('*', '')}</strong>
+								borough.map((value: string[]) => (
+									<div key={value[0]}>
+										<strong>{value[0]}</strong>
 										:
 										{' '}
-										<NumberValue value={section[1]} />
+										<NumberValue value={value[1]} />
 									</div>
 								))
 							}
 							<br />
 							<h2 className="font-bold text-xl">Case Rate By Age:</h2>
 							{
-								//	eslint-disable-next-line @typescript-eslint/no-explicit-any
-								age.map((section: any) => (
-									<div key={section[0]}>
-										<strong>{section[0].replace('*', '')}</strong>
+								age.map((value: string[]) => (
+									<div key={value[0]}>
+										<strong>{value[0]}</strong>
 										:
 										{' '}
-										<NumberValue value={section[1]} />
+										<NumberValue value={value[1]} />
 									</div>
 								))
 							}
 							<br />
 							<h2 className="font-bold text-xl">Case Rate By Sex:</h2>
 							{
-								//	eslint-disable-next-line @typescript-eslint/no-explicit-any
-								sex.map((section: any) => (
-									<div key={section[0]}>
-										<strong>{section[0].replace('*', '')}</strong>
+								sex.map((value: string[]) => (
+									<div key={value[0]}>
+										<strong>{value[0]}</strong>
 										:
 										{' '}
-										<NumberValue value={section[1]} />
+										<NumberValue value={value[1]} />
 									</div>
 								))
 							}
@@ -249,12 +248,11 @@ class Visualization extends React.Component<MyProps, MyState> {
 							<h2 className="font-bold mb-3 text-xl">New York City:</h2>
 							<div className="mb-6">
 								{
-									//	eslint-disable-next-line @typescript-eslint/no-explicit-any
-									totals.map((section: any) => (
-										<div key={section[0]}>
-											<strong>{section[0].replace('*', '')}</strong>
+									totals.map((value: string[]) => (
+										<div key={value[0]}>
+											<strong>{value[0]}</strong>
 											{' '}
-											<NumberValue value={section[1]} />
+											<NumberValue value={value[1]} />
 										</div>
 									))
 								}
